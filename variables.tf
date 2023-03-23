1db7ee61ae65b465091a302d0a98d98efcf66759
chriszara <62260728+chriszara@users.noreply.github.com>
2023-03-22 20:29:58 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "mySep22Key" 
+    default     =  "MyJenkins2023key" 
 }
 
 variable "instance_type" { 
